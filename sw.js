dc9074baf17eda1dc779089e85a3f64b373be510
fdavidsen <f.davidsen19@gmail.com>
2020-08-22 09:31:54 +0700

    Change background color and add static assets

--- sw.js
+++ sw.js
@@ -1,6 +1,21 @@
 const staticAssets = [
 	'./',
 	'./index.html',
+	'./assets/img/Aries.jpg',
+	'./assets/img/Taurus.jpg',
+	'./assets/img/Gemini.jpg',
+	'./assets/img/Cancer.jpg',
+	'./assets/img/Leo.jpg',
+	'./assets/img/Virgo.jpg',
+	'./assets/img/Libra.jpg',
+	'./assets/img/Scorpio.jpg',
+	'./assets/img/Sagittarius.jpg',
+	'./assets/img/Capricorn.jpg',
+	'./assets/img/Aquarius.jpg',
+	'./assets/img/Pisces.jpg',
+	'./assets/js/functions.js',
+	'./assets/js/mapping.js',
+	'./assets/js/script.js',
 	'./assets/style.css'
 ];
 
